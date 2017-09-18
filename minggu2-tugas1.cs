0a642fca1f6c87cf357c5834b5904baa472e3bc0
Muhamad Danil Rafiqi <danil.rafiqi@gmail.com>
2017-09-18 15:01:16 +0700

    tugas minggu kedua, luas segitiga dan perkenalan

---
+++ minggu2-tugas1.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace minggu2_tugas1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a, t;
+            double luas;
+            Console.WriteLine("==Menghitung luas segitiga==");
+            Console.Write("Masukkan alas : ");
+            a = int.Parse(Console.ReadLine());
+            Console.Write("Masukkan tinggi : ");
+            t = int.Parse(Console.ReadLine());
+            luas = a * t / 2.0;
+            Console.WriteLine("Luas Segitiga yang alasnya "+ a + " dan tingginya "+ t+" adalah "+luas);
+            Console.ReadLine();
+        }
+    }
+}
---
+++ minggu2-tugas2.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace minggu2_tugas2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string nama, npm, ps, alamat, hp;
+            Console.WriteLine("DATA DIRI");
+            Console.WriteLine("Isilah Data dibawah ini :");
+            Console.Write("Nama          : ");
+            nama = Console.ReadLine();
+            Console.Write("NPM           : ");
+            npm = Console.ReadLine();
+            Console.Write("Program Studi : ");
+            ps = Console.ReadLine();
+            Console.Write("Alamat        : ");
+            alamat = Console.ReadLine();
+            Console.Write("No HP         : ");
+            hp = Console.ReadLine();
+            Console.WriteLine("Halo "+nama+" NPM kamu "+npm+" Program Studi kamu "+ps+", Alamat kamu "+alamat+" No telepon kamu "+hp);
+            Console.ReadLine();
+
+        }
+    }
+}
