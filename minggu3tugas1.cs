8105ace0119ff7b19256aac92e99c6d915bf915f
Muhamad Danil Rafiqi <danil.rafiqi@gmail.com>
2017-09-25 14:12:28 +0700

    Add files via upload

---
+++ minggu3tugas1.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace minggu3tugas1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int kuis, uts, uas, total;
+            double rata;
+            Console.WriteLine("===Menghitung Nilai===");
+            Console.Write("Masukan nilai KUIS: ");
+            kuis = int.Parse(Console.ReadLine());
+            Console.Write("Masukan nilai UTS: ");
+            uts = int.Parse(Console.ReadLine());
+            Console.Write("Masukan nilai UAS: ");
+            uas = int.Parse(Console.ReadLine());
+
+            total = kuis + uas + uts;
+            rata = total / 3.0;
+            Console.WriteLine("Total Nilai adalah: " + total);
+            Console.WriteLine("Rata Rata Nilai adalah: " + rata);
+            if (rata >= 80)
+            {
+                Console.WriteLine("Kamu mendapat Nilai A");
+            }
+            else if (rata >= 60)
+            {
+                Console.WriteLine("Kamu mendapat Nilai B");
+            }
+            else
+            {
+                Console.WriteLine("Kamu mendapat Nilai C");
+            }
+            Console.Read();
+        }
+    }
+}
---
+++ minggu3tugas2.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace minggu3tugas2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string nama, nip, status;
+            int masa, tunjangannikah = 0, gaji, tunjanganmasa = 0, gajiakhir;
+
+            Console.WriteLine("===Menghitung Gaji===");
+            Console.Write("Masukan Nama                    : ");
+            nama = Console.ReadLine();
+            Console.Write("Masukan Nip                     : ");
+            nip = Console.ReadLine();
+            Console.Write("Masukan Gaji Pokok              : ");
+            gaji = int.Parse(Console.ReadLine());
+            Console.Write("Masukan Lama Masa Kerja (Tahun) : ");
+            masa = int.Parse(Console.ReadLine());
+            if (masa >= 5)
+            {
+                tunjanganmasa = 20 * gaji / 100;
+            }
+            Console.Write("Apakah Kamu sudah menikah (y/n) : ");
+            status = Console.ReadLine();
+            status.ToLower();
+
+            if (status == "y" || status == "ya" || status == "yes")
+            {
+                tunjangannikah = 10 * gaji / 100;
+            }
+
+            gajiakhir = gaji + tunjanganmasa + tunjangannikah;
+
+            Console.WriteLine("==================================");
+            Console.WriteLine("Nama                 : "+nama);
+            Console.WriteLine("Gaji Pokok           : " + gaji);
+            Console.WriteLine("Tunjangan Nikah      : " + tunjangannikah);
+            Console.WriteLine("Tunjangan Masa Kerja : " + tunjanganmasa);
+            Console.WriteLine("Gaji Akhir           : " + gajiakhir);
+            Console.Read();
+        }
+    }
+}
