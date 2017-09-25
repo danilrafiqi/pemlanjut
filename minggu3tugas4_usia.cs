d86efc6e4d57daa1940907280d504111a8190499
Muhamad Danil Rafiqi <danil.rafiqi@gmail.com>
2017-09-25 16:16:47 +0700

    Add files via upload

---
+++ minggu3tugas4_usia.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace minggu3tugas4_usia
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int usia;
+            Console.WriteLine("=========================");
+            Console.WriteLine("      Hitung Usia");
+            Console.WriteLine("=========================");
+            Console.Write("Masukan Usia : ");
+            usia = int.Parse(Console.ReadLine());
+            if (usia <= 21) Console.WriteLine("Kamu masih anak anak");
+            else if (usia > 21 && usia <= 40) Console.WriteLine("Kamu sudah dewasa");
+            else Console.WriteLine("Kamu sudah tua");
+            Console.ReadLine();
+        }
+    }
+}
---
+++ minggu3tugas5_nilai.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace minggu3tugas5_nilai
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int nilai;
+            Console.WriteLine("=========================");
+            Console.WriteLine("           Nilai");
+            Console.WriteLine("=========================");
+            Console.Write("Masukan Nilai : ");
+            nilai = int.Parse(Console.ReadLine());
+            Console.WriteLine();
+            if (nilai > 80) Console.WriteLine("Nilai Kamu "+nilai+" Selamat Kamu Mendapat Nilai A");
+            else if (nilai > 60) Console.WriteLine("Nilai Kamu " + nilai + " Kamu Mendapat Niali B");
+            else Console.WriteLine("Nilai Kamu " + nilai + " Kamu Mendapat Nilai C");
+            Console.ReadLine();
+        }
+    }
+}
